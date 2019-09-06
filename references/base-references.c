2f3a878577179e518e81afcabcf4d447438af657
TheSignPainter <ed@kcza.net>
2019-09-06 17:55:48 +0100

    Made initial implementation

--- references/base-references.c
+++ references/base-references.c
@@ -1,3 +1,110 @@
 #include "base-references.h"
 
-// TODO: Pointers as structs to allow immutable data-structures
+base_Any_t base_dereference(base_Reference_t rootRef, base_ReferenceContext_t ctx)
+{
+	base_Any_t* toReturn             = NULL;
+	base_ReferenceContext_t* currCtx = &ctx;
+
+	// Walk up the contexts until a change is seen
+	while (true)
+	{
+		// Search down the reference tree to find whether a change has been made
+		toReturn = getReferenceValueByIndex(&rootRef, currCtx->idx);
+		if (toReturn != NULL)
+		{
+			return *toReturn;
+		}
+		else
+		{
+			currCtx = currCtx->parent;
+		}
+	}
+
+	return *toReturn;
+}
+
+base_Any_t* getReferenceValueByIndex(base_Reference_t* ref, base_ReferenceIndex_t idx)
+{
+	// Walk down the reference tree
+	while (ref != NULL)
+	{
+		if (ref->idx < idx)
+		{
+			ref = ref->leftChild;
+		}
+		else if (ref->idx > idx)
+		{
+			ref = ref->rightChild;
+		}
+		else // ref.idxNode == idx
+		{
+			return &ref->value;
+		}
+	}
+	return NULL;
+}
+
+base_ReferenceContext_t* base_makeNewContext(base_ReferenceContext_t* parent)
+{
+	base_ReferenceContext_t* toReturn = (base_ReferenceContext_t*)malloc(sizeof(base_ReferenceContext_t));
+	toReturn->parent                  = parent;
+
+	// Generate the index of the new context;
+	// Thanks, Uli Schlachter! https://stackoverflow.com/questions/3665257/generate-random-long-number
+	base_ReferenceIndex_t idx = 0x0;
+	for (long unsigned int i = 0; i < sizeof(base_ReferenceIndex_t) / sizeof(int); i++)
+	{
+		idx = (idx << (sizeof(int) * 8)) | rand();
+	}
+
+	toReturn->idx = idx;
+
+	return toReturn;
+}
+
+base_Reference_t base_makeNewReference(base_ReferenceContext_t ctx, base_Any_t value)
+{
+	base_Reference_t ref;
+	ref.canBeFreed = false;
+	ref.leftChild  = NULL;
+	ref.rightChild = NULL;
+	ref.value      = value;
+	ref.idx        = ctx.idx;
+	return ref;
+}
+
+void base_authorReferenceChange(base_Reference_t ref, base_ReferenceContext_t ctx, base_Any_t value)
+{
+	base_ReferenceIndex_t idx = ctx.idx;
+	base_Reference_t* currRef = &ref;
+	base_Reference_t* nextRef = NULL;
+
+	while (true)
+	{
+		if (currRef->idx < idx)
+		{
+			nextRef = currRef->leftChild;
+			if (nextRef == NULL)
+			{
+				base_Reference_t newRef = base_makeNewReference(ctx, value);
+				currRef->leftChild      = &newRef;
+				return;
+			}
+			currRef = nextRef;
+		}
+		else if (currRef->idx > idx)
+		{
+			nextRef = currRef->rightChild;
+			if (nextRef == NULL)
+			{
+				base_Reference_t newRef = base_makeNewReference(ctx, value);
+				currRef->rightChild     = &newRef;
+				return;
+			}
+		}
+		else
+		{
+			currRef->value = value;
+		}
+	}
+}
--- references/base-references.h
+++ references/base-references.h
@@ -2,13 +2,31 @@
 #define BASE_REFERENCES_H_
 
 #include "../generics/base-generics.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef const long int base_ReferenceIndex_t;
+
+typedef struct base_referenceContext
+{
+	base_ReferenceIndex_t idx;
+	struct base_referenceContext* parent;
+} base_ReferenceContext_t;
 
 typedef struct base_reference
 {
 	base_Any_t value;
-	long int idx;
+	base_ReferenceIndex_t idx;
+	struct base_reference* leftChild;
+	struct base_reference* rightChild;
+	bool canBeFreed;
 } base_Reference_t;
 
-void* base_getValue(base_Reference_t, int);
+base_Any_t base_dereference(base_Reference_t, base_ReferenceContext_t);
+static base_Any_t* getReferenceValueByIndex(base_Reference_t*, base_ReferenceIndex_t);
+
+base_ReferenceContext_t* base_makeNewContext(base_ReferenceContext_t*);
+base_Reference_t base_makeNewReference(base_ReferenceContext_t, base_Any_t);
+void base_authorReferenceChange(base_Reference_t, base_ReferenceContext_t, base_Any_t);
 
 #endif /* BASE_REFERENCES_H_ */
